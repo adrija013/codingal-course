bd0ed4be5a06883e7392216d23498469f207a788
Adrija013 <adrijabondyopadhyay9604@gmail.com>
2025-09-17 22:14:41 +0530

    initial commit

---
+++ m8/cw activity/L2-A2.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow" }
+print("Original set elements:")
+print(setx)
+print(sety)
+setz = sety.intersection(setx)
+print("\nIntersection of above sets:")
+print(setz)
