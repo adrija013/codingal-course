135897821ae756e75cb4bb3b433f0420923953ad
Adrija013 <adrijabondyopadhyay9604@gmail.com>
2025-10-11 22:19:35 +0530

    initial commit

---
+++ m9/cw activity/L4-A2.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.configure(bg="red")
+for i in range(3):
+    for j in range(3):
+        frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=5)
+        frame.grid(row=i, column=j, padx=3, pady=3)
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack()
+window.mainloop()        
