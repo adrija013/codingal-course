49f63cfc86531732d3c480cf665b4557d46ee664
Adrija013 <adrijabondyopadhyay9604@gmail.com>
2025-11-22 14:45:20 +0530

    initial commit

---
+++ m11/L2-A2.py
@@ -0,0 +1,9 @@
+def setOrnot(number, n):
+    if number & (1 << (n-1)):
+        print("SET")
+    else:
+         print("NOT SET")   
+
+number = int(input("Enter the number: "))
+n = int(input("Enter the bit position: "))   
+setOrnot(number, n)     
